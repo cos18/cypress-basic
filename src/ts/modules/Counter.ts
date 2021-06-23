8ef98cbeb1973816d059aa00ba0964a41fe2bbdf
Sungwoo Park <codest99@gmail.com>
2021-06-23 23:34:00 +0900

    chore: plus-button 기능 작동

--- src/ts/modules/Counter.ts
+++ src/ts/modules/Counter.ts
@@ -1,19 +1,33 @@
 export default function Counter({ $app }: { $app: HTMLElement }) {
+  let countValue = 10;
+  let countDisplay: HTMLInputElement | null;
+
   const render = () => {
     $app.innerHTML = `
       <div class="container">
         <h1>ui counter</h1>
         <div class="counter">
-          <a href="#" class="minus-button"><span>-</span></a>
+          <a class="minus-button"><span>-</span></a>
           <input name="count" type="text" class="count-display" value="10">
-          <a href="#" class="plus-button"><span>+</span></a>
+          <a class="plus-button"><span>+</span></a>
         </div>
       </div>
     `;
   };
 
+  const onPlusButtonClicked = () => {
+    if (countValue < 12) {
+      countValue += 1;
+    }
+    if (countDisplay) {
+      countDisplay.value = countValue.toString();
+    }
+  };
+
   const init = () => {
     render();
+    countDisplay = document.querySelector('.count-display');
+    document.querySelector('.plus-button')?.addEventListener('click', onPlusButtonClicked);
   };
 
   init();
