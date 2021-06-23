887354a6becb2995b7d09dabd3c7813862caf277
Sungwoo Park <codest99@gmail.com>
2021-06-23 23:14:00 +0900

    chore: 작동 관련 테스트 추가

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -2,10 +2,11 @@ describe('Counter module', () => {
   it('renders components', () => {
     cy.visit('/');
     cy.get('.container > h1').contains('ui counter');
-    cy.get('.minus-button');
-    cy.get('.plus-button');
+    cy.get('.minus-button').contains('-');
+    cy.get('.plus-button').contains('+');
     cy.get('.count-display').should('have.value', 10);
   });
+
   it('increases count value when click plus-button', () => {
     cy.reload();
     cy.get('.count-display').should('have.value', 10);
@@ -13,5 +14,30 @@ describe('Counter module', () => {
       .get('.count-display').should('have.value', 11);
     cy.get('.plus-button').click()
       .get('.count-display').should('have.value', 12);
+  });
+
+  it('doesn\'t increase over 12', () => {
+    cy.get('.plus-button').click()
+      .get('.count-display').should('have.value', 12);
+    cy.get('.plus-button').click().click().click()
+      .get('.count-display')
+      .should('have.value', 12);
+  });
+
+  it('decreases count value when click minus-button', () => {
+    cy.reload();
+    cy.get('.count-display').should('have.value', 10);
+    cy.get('.minus-button').click()
+      .get('.count-display').should('have.value', 9);
+    cy.get('.minus-button').click()
+      .get('.count-display').should('have.value', 8);
+  });
+
+  it('doesn\'t decrease over 8', () => {
+    cy.get('.minus-button').click()
+      .get('.count-display').should('have.value', 8);
+    cy.get('.minus-button').click().click().click()
+      .get('.count-display')
+      .should('have.value', 8);
   });
 });
