3b326880ebd0f104e6f6299a12bd3260189efd88
Sungwoo Park <codest99@gmail.com>
2021-06-23 23:06:39 +0900

    chore: 컴포넌트 랜더링 관련 테스트 추가

--- cypress/integration/index.spec.ts
+++ cypress/integration/index.spec.ts
@@ -1,5 +1,17 @@
-describe('My first test', () => {
-  it('is fail', () => {
-    expect(true).to.equal(false);
+describe('Counter module', () => {
+  it('renders components', () => {
+    cy.visit('/');
+    cy.get('.container > h1').contains('ui counter');
+    cy.get('.minus-button');
+    cy.get('.plus-button');
+    cy.get('.count-display').should('have.value', 10);
+  });
+  it('increases count value when click plus-button', () => {
+    cy.reload();
+    cy.get('.count-display').should('have.value', 10);
+    cy.get('.plus-button').click()
+      .get('.count-display').should('have.value', 11);
+    cy.get('.plus-button').click()
+      .get('.count-display').should('have.value', 12);
   });
 });
--- src/ts/modules/Counter.ts
+++ src/ts/modules/Counter.ts
@@ -1,14 +1,15 @@
 export default function Counter({ $app }: { $app: HTMLElement }) {
   const render = () => {
     $app.innerHTML = `
-       <div class="container">
-          <h1>ui counter</h1>
-          <div class="counter">
-            <a href="#" class="minus-button"><span>-</span></a>
-            <input name="count" type="text" class="count-display" value="10">
-            <a href="#" class="plus-button"><span>+</span></a>
-          </div>
-        </div>`;
+      <div class="container">
+        <h1>ui counter</h1>
+        <div class="counter">
+          <a href="#" class="minus-button"><span>-</span></a>
+          <input name="count" type="text" class="count-display" value="10">
+          <a href="#" class="plus-button"><span>+</span></a>
+        </div>
+      </div>
+    `;
   };
 
   const init = () => {
